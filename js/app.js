c415affc58fc272c52c67ce13bbc6f8abb3804bb
kyle715 <kyle7150@gmail.com>
2021-06-10 14:01:03 -0400

    changed styling and added some more scenarios

--- js/app.js
+++ js/app.js
@@ -24,26 +24,216 @@ const moneyProgress = document.getElementById('money')
 
 const displayCardArea = document.querySelector('.card')
 
+const game = document.getElementById('game')
+
+const startArea = document.querySelector('.startButtonArea')
+
+
 let cardNumber = 0
 
 // OBJECTS
 
 let moneyValue = {
-    value: 50
+    value: 20
 }
 
 let knowledgeValue = {
-    value: 50
+    value: 20
 } 
 
 let socialValue = {
-    value: 50
+    value: 20
 }
 
+
+//CARDS
+
+const cards = [
+    {
+        context: 'You were planning on studying this weekend but you recived an invite on linkedIn to an event going on this weekend for local developers',
+        button1Social: 20,
+        button1Knowledge: 5,
+        button1Money: -5,
+        button2Social: -5,
+        button2Knowledge: 10,
+        button2Money: 5,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'Go to the event!',
+        button2Text: 'Best to study...',
+    },
+    {
+        context: 'You are offered a small freelance project to build a program that helps a local company keep track of their customers',
+        button1Social: 1,
+        button1Knowledge: 10,
+        button1Money: 20,
+        button2Social: 1,
+        button2Knowledge: 1,
+        button2Money: 1,
+        cardIMG: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTl3xXBxcRYm0FdEenBYLT3PM-DnLx0Ec2zQ&usqp=CAU',
+        button1Text: 'Id be happy to take the job',
+        button2Text: 'Sorry, im busy.',
+    },
+    {
+        context: 'Your friends are joining at a local library to put together a study group, but you were planing on working on a freelance project',
+        button1Social: 15,
+        button1Knowledge: 15,
+        button1Money: 0,
+        button2Social: -5,
+        button2Knowledge: 10,
+        button2Money: 15,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'Id love to join!',
+        button2Text: 'I need the money.',
+    },
+    {
+        context: "You are interested in taking a Udemy course but the cost of it is a bit high. This will certainly help you out in the long run... but is it worth it",
+        button1Social: 0,
+        button1Knowledge: 15,
+        button1Money: -20,
+        button2Social: 0,
+        button2Knowledge: 0,
+        button2Money: 5,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'Lets take it',
+        button2Text: "I'll pass",
+    },
+    {
+        context: "Your Aunt hears about you trying to become a software engineer, she asks that instead of working on a project this weekend you come by to help her set up her printer",
+        button1Social: 10,
+        button1Knowledge: 0,
+        button1Money: 10,
+        button2Social: 0,
+        button2Knowledge: 10,
+        button2Money: 0,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'Sure, ill help',
+        button2Text: 'Sorry, cars in the shop',
+    },
+    {
+        context: "Bills are due",
+        button1Social: 0,
+        button1Knowledge: 0,
+        button1Money: -20,
+        button2Social: 0,
+        button2Knowledge: 0,
+        button2Money: -20,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'PAY UP',
+        button2Text: 'YOU HEARD HIM',
+    },
+    {
+        context: "Someone from a local brewery has reached out to you on LinkedIn to play a part in writing the program to sell their new line of beer 'Penquin Power Ale' online",
+        button1Social: 5,
+        button1Knowledge: 15,
+        button1Money: 30,
+        button2Social: 5,
+        button2Knowledge: 15,
+        button2Money: 30,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'Id be happy to work with you',
+        button2Text: 'Do i get some free samples?',
+    },
+    {
+        context: "Pop Quiz: What does DOM stand for",
+        button1Social: 0,
+        button1Knowledge: 20,
+        button1Money: 0,
+        button2Social: 0,
+        button2Knowledge: -20,
+        button2Money: 0,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'Document Orientation Model',
+        button2Text: 'Document Object Model',
+    },
+    {
+        context: "While working on the program for the local brewery you get a phonecall from Mom and she rants on about her day for what seems like forever... now what was the name of that new beer again?",
+        button1Social: 20,
+        button1Knowledge: 10,
+        button1Money: 0,
+        button2Social: -20,
+        button2Knowledge: -10,
+        button2Money: 0,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'Penquin Power Ale',
+        button2Text: 'Penquins Pickup Drink',
+    },
+    {
+        context: "win card",
+        button1Social: 50,
+        button1Knowledge: 50,
+        button1Money: 50,
+        button2Social: -50,
+        button2Knowledge: -50,
+        button2Money: -50,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'win',
+        button2Text: 'lose',
+    },
+    {
+        context: "win card",
+        button1Social: 50,
+        button1Knowledge: 50,
+        button1Money: 50,
+        button2Social: -50,
+        button2Knowledge: -50,
+        button2Money: -50,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'win',
+        button2Text: 'lose',
+    },
+    {
+        context: "win card",
+        button1Social: 50,
+        button1Knowledge: 50,
+        button1Money: 50,
+        button2Social: -50,
+        button2Knowledge: -50,
+        button2Money: -50,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'win',
+        button2Text: 'lose',
+    },
+    {
+        context: "win card",
+        button1Social: 50,
+        button1Knowledge: 50,
+        button1Money: 50,
+        button2Social: -50,
+        button2Knowledge: -50,
+        button2Money: -50,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'win',
+        button2Text: 'lose',
+    },
+    {
+        context: "win card",
+        button1Social: 50,
+        button1Knowledge: 50,
+        button1Money: 50,
+        button2Social: -50,
+        button2Knowledge: -50,
+        button2Money: -50,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'win',
+        button2Text: 'lose',
+    },
+    {
+        context:'You are ready to apply for a job, you send out your resume to your favorite local company and wish for the best',
+        button1Social: 0,
+        button1Knowledge: 0,
+        button1Money: 0,
+        button2Social: 0,
+        button2Knowledge: 0,
+        button2Money: 0,
+        cardIMG: 'https://previews.123rf.com/images/saphatthachat/saphatthachat1803/saphatthachat180300162/98031934-vector-pixel-art-book-open-isolated-cartoon.jpg',
+        button1Text: 'I hope i get it...',
+        button2Text: 'but what if i dont?',
+    }
+]
+
+
+
 // FUNCTIONS
-
-// let randomCard = Math.floor(Math.random() * 4)
-
 
 
 const updateSocial = (value) => {
@@ -61,71 +251,70 @@ const updateMoney = (value) => {
     money.innerHTML = `Money :${moneyValue.value}/100`
 }
 
-
-//CARDS
-
-const cards = [
-    {
-        context: 'You were planning on studying this weekend but you recived an invite on linkedIn to an event going on this weekend for local developers',
-        button1Social: 20,
-        button1Knowledge: 5,
-        button1Money: -5,
-        button2Social: -5,
-        button2Knowledge: 20,
-        button2Money: 5,
-        button1Text: 'Go to the event!',
-        button2Text: 'Best to study...',
-    },
-    {
-        context: 'You are offered a small freelance project to build a program that helps a local company keep track of their customers',
-        button1Social: 1,
-        button1Knowledge: 10,
-        button1Money: 30,
-        button2Social: 1,
-        button2Knowledge: 1,
-        button2Money: 1,
-        button1Text: 'Id be happy to take the job',
-        button2Text: 'Sorry, im busy.',
-    },
-    {
-        context: 'Your friends are joining at a local library to put together a study group, but you were planing on working on a freelance project',
-        button1Social: 20,
-        button1Knowledge: 20,
-        button1Money: 0,
-        button2Social: -5,
-        button2Knowledge: 1,
-        button2Money: 30,
-        button1Text: 'Id love to join!',
-        button2Text: 'I need the money.',
-    },
-    {
-        context: "win card",
-        button1Social: 50,
-        button1Knowledge: 50,
-        button1Money: 50,
-        button2Social: -50,
-        button2Knowledge: -50,
-        button2Money: -50,
-        button1Text: 'win',
-        button2Text: 'lose',
-    },
-    {
-        context:'You are ready to apply for a job, you send out your resume to your favorite local company and wish for the best',
-        button1Social: 0,
-        button1Knowledge: 0,
-        button1Money: 0,
-        button2Social: 0,
-        button2Knowledge: 0,
-        button2Money: 0,
-        button1Text: 'I hope i get it...',
-        button2Text: 'but what if i dont?',
+const createCards = () => {
+    for (let i = 0; i < cards.length; i++) {
+        let createCards = document.createElement('div')
+        createCards.setAttribute('class', 'game--card')
+        let newImg = document.createElement('img')
+        newImg.setAttribute('src', `${cards[i].cardIMG}`)
+        createCards.appendChild(newImg)
+        let newP = document.createElement('p')
+        newP.setAttribute('class', 'pContext')
+        newP.innerHTML = `${cards[i].context}`
+        createCards.appendChild(newP)
+    
+        displayCardArea.appendChild(createCards)
+        
     }
-]
+}
 
-const startGame = () => {
-    startGameButton.style.display ='none'
-    buttonOne.setAttribute('class', 'buttonStyle')
-    buttonTwo.setAttribute("class", 'buttonStyle')
+
+const initCards = (card, index) => {
+    var newCards = document.querySelectorAll('.game--card:not(.removed)');
+  
+    newCards.forEach(function (card, index) {
+      card.style.zIndex = cards.length - index;
+      card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+      card.style.opacity = (10 - index) / 10;
+    });
+    
+    displayCardArea.classList.add('loaded');
+  }
+  
+
+  function createButtonListener(love) {
+    return function (event) {
+      var allCards = document.querySelectorAll('.game--card:not(.removed)');
+      var moveOutWidth = document.body.clientWidth * 1.5;
+      console.log(allCards)
+  
+      if (!cards.length) return false;
+  
+      var card = allCards[0];
+  
+      card.classList.add('removed');
+  
+      if (love) {
+        card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+        console.log('hi')
+      } else {
+        card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+        console.log('ohai')
+      }
+  
+      initCards();
+  
+      event.preventDefault();
+    };
+  }
+
+  const startGame = () => {
+    startArea.style.display ='none'
+    createCards()
+    initCards()
+    buttonOne.setAttribute('class', 'nes-btn is-warning')
+    buttonTwo.setAttribute("class", 'nes-btn is-warning')
+    game.setAttribute('class', 'game')
     buttonOne.innerText = `${cards[cardNumber].button1Text}`
     buttonTwo.innerText = `${cards[cardNumber].button2Text}`
     buttonOne.addEventListener('click',(e) => {
@@ -135,6 +324,7 @@ const startGame = () => {
     playCards(e)
     })
 }
+  
 
 const playCards = (e) => {
     if(cardNumber + 1 === cards.length) {
@@ -170,81 +360,23 @@ const playCards = (e) => {
 
 const endGame = () => {
     if(socialValue.value > 80 && knowledgeValue.value > 80 && moneyValue.value > 80) {
-        card.innerHTML = 'you got the job!'
+        card.innerHTML = '<div class="nes-container is-rounded is-dark"><p>You got the job!</p></div>'
     } else {
-        card.innerHTML = 'better luck next time...'
+        card.innerHTML = '<p>better luck next time...</p>'
     }
 }
 
-const createCards = () => {
-    for (let i = 0; i < cards.length; i++) {
-        let createCards = document.createElement('div')
-        createCards.setAttribute('class', 'game--card')
-        let newh3 = document.createElement('h3')
-        newh3.innerHTML = 'card title'
-        createCards.appendChild(newh3)
-        let newP = document.createElement('p')
-        newP.innerHTML = `${cards[i].context}`
-        createCards.appendChild(newP)
+
+
     
-        displayCardArea.appendChild(createCards)
-        
-    }
-}
-createCards()
+  
+  //MISC
+const noListener = createButtonListener(false);
+const yesListener = createButtonListener(true);
 
-
-const initCards = (card, index) => {
-    var newCards = document.querySelectorAll('.game--card:not(.removed)');
+buttonOne.addEventListener('click', noListener);
+buttonTwo.addEventListener('click', yesListener);
   
-    newCards.forEach(function (card, index) {
-      card.style.zIndex = cards.length - index;
-      card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
-      card.style.opacity = (10 - index) / 10;
-    });
-    
-    displayCardArea.classList.add('loaded');
-    // displayCardArea.setAttribute('class', 'loaded')
-  }
-  
-  initCards();
-
-  function createButtonListener(love) {
-    return function (event) {
-      var allCards = document.querySelectorAll('.game--card:not(.removed)');
-      var moveOutWidth = document.body.clientWidth * 1.5;
-      console.log(allCards)
-  
-      if (!cards.length) return false;
-  
-      var card = allCards[0];
-  
-      card.classList.add('removed');
-  
-      if (love) {
-        card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
-        console.log('hi')
-      } else {
-        card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
-        console.log('ohai')
-      }
-  
-      initCards();
-  
-      event.preventDefault();
-    };
-  }
-  
-  
-  var noListener = createButtonListener(false);
-  var yesListener = createButtonListener(true);
-
-  buttonOne.addEventListener('click', noListener);
-  buttonTwo.addEventListener('click', yesListener);
-
-
-//MISC
-
 const startButtonGame = document.getElementById('startGameButton')
 
 startButtonGame.addEventListener('click', startGame)
